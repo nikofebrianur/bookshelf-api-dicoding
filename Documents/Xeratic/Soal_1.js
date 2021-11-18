8590ad72c35b8234088bcbfd0ee40159357b9f1f
Niko Febrianur <nikofebrianur@gmail.com>
2021-11-18 13:17:55 +0800

    soal 1

---
+++ Documents/Xeratic/Soal_1.js
@@ -0,0 +1,28 @@
+const usedCombination = [
+    'one', 
+    'who', 
+    'two', 
+    'bob', 
+    'add', 
+    'owl', 
+    'fab', 
+    'den', 
+    'mia', 
+    'tat'
+]
+
+// masukkan tiap letter ke dial
+const letter1 = []
+const letter2 = []
+const letter3 = []
+
+// fungsi untuk mencocokkan input
+function Lock(input) {
+    // split input
+    const splitInput = input.split 
+    // cocokkan tiap letter berdasar dial
+    
+    // jika letter tidak ada di dial, return false
+}
+
+console.log(Lock(hat))
